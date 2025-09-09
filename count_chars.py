d0959b8d2d8c9a7ecefd18f2917875517ab6bc77
Landon Butler <landonbutler@gmail.com>
2025-09-09 15:12:38 -0600

    Update count_chars.py

--- count_chars.py
+++ count_chars.py
@@ -1,7 +1,7 @@
-#This function will cound the number of a specific character in a given string.
+#This function will count the number of a specific character in a given string.
 def count_character(string, character):
-    string = string.lower()
-    character = character.lower()
+    string = string.lower().strip()
+    character = character.lower().strip()
     count = 0
     for char in string:
         if char == character:
@@ -9,9 +9,9 @@ def count_character(string, character):
     return count
 
 
-input_string = input("Enter a string: ".strip())
+input_string = input("Enter a string: ")
 
-input_character = input("Enter a character to count: ".strip())
+input_character = input("Enter a character to count: ")
 result = count_character(input_string, input_character)
 print(f"The character '{input_character}' appears {result} times in the string.")
 input("Press Enter to exit")
